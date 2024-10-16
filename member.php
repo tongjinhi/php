248118440b934852a561b1f8fe32e7ebb9479d1a
tongjinhi <148730050+tongjinhi@users.noreply.github.com>
2024-10-16 12:03:10 +0900

    Update member.php

--- member.php
+++ member.php
@@ -12,15 +12,15 @@
             <tr><td>아이디</td>
                 <td><input type="text" size="35" name="fid" placeholder="아이디"> </td></tr>
             <tr><td>비밀번호</td>
-            <td><input type="password" size="35" name="fpass" placeholder="비밀번호"></td></tr>
+                <td><input type="password" size="35" name="fpass" placeholder="비밀번호"></td></tr>
             <tr><td>이름</td>
-            <td><input type="text" size="35" name="fname" placeholder="이름"> </td></tr>
+                <td><input type="text" size="35" name="fname" placeholder="이름"> </td></tr>
             <tr><td>주소</td>
-            <td><input type="text" size="35" name="faddr" placeholder="주소"> </td></tr>
+                <td><input type="text" size="35" name="faddr" placeholder="주소"> </td></tr>
             <tr><td>성별</td>
-            <td>남<input type="radio" name="fgender" value="남"> 여<input type="radio" name="fgender" value="여"></td></tr>
+                <td>남<input type="radio" name="fgender" value="남"> 여<input type="radio" name="fgender" value="여"></td></tr>
             <tr><td>이메일</td>
-            <td><input type="text" size="35" name="femail" placeholder="주소"> </td></tr>
+                <td><input type="text" size="35" name="femail" placeholder="주소"> </td></tr>
         </table>
         <input type="submit" value="가입하기"/>
         <input type="reset" value="다시쓰기"/>
