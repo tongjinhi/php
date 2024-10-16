e595904c41af8aa365b656bf979471af1f316951
tongjinhi <148730050+tongjinhi@users.noreply.github.com>
2024-10-16 12:32:07 +0900

    Add files via upload

---
+++ 21101/member_ok.php
@@ -0,0 +1,16 @@
+<?php
+    include "./db.php";
+    $userid = $_POST['fid'];
+    $userpw = password_hash($_POST['fpass'], PASSWORD_DEFAULT);
+    $username = $_POST['fname'];
+    $addr = $_POST['faddr'];
+    $gender = $_POST['fgender'];
+    $email = $_POST['femail'];
+
+    $sql = my_query("insert into testmember (id,pass,name,address,gender,email) values('
+    ".$userid."','".$userpw."','".$username."','".$addr."','".$gender."','".$email."')");
+?>
+<script type="text/javascript">alert('회원가입이 완료되었습니다.'); 
+        location.href="index.php"
+</script>
+<meta http-equiv="refresh" content="0 url=/">
