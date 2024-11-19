7fb78e1de451d782c5a65cf057bad8653615cf3b
tongjinhi <148730050+tongjinhi@users.noreply.github.com>
2024-11-19 15:54:11 +0900

    Add files via upload

---
+++ 20423/game_search.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8" />
+    <link type="text/css" rel="stylesheet" href="book.css">
+</head>
+<body>
+    <h1>게임 검색</h1>
+    <div style="text-align: center;">
+        <form action="game_search_ok.php" method="POST">
+            <fieldset>
+                <select name="search_option">
+                    <option value="name">        게임 이름</option>
+                    <option value="P_date">      출시일   </option>
+                    <option value="Genre">       장르     </option>
+                    <option value="rating">      연령 제한</option>
+                    <option value="explanation"> 게임 설명</option>
+                </select>
+                <input type="text" name="search_word">
+                <input type="submit" value="검색"> 
+            </fieldset>
+        </form>
+    </div>
+</body>
+</html>
