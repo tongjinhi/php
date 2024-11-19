10afc8a02fb26617a3051fd12c09fe305c849bae
tongjinhi <148730050+tongjinhi@users.noreply.github.com>
2024-11-19 16:22:07 +0900

    Update burger_search.php

--- 20415/burger_search.php
+++ 20415/burger_search.php
@@ -3,7 +3,7 @@
 <html>
     <head>
         <meta charset="utf-8"/>
-        <link type="text/css" rel="stylesheet" href="book.css">
+        <link type="text/css" rel="stylesheet" href="../book.css">
     </head>
     <body>
         <h1> 버거 검색 </h1>
