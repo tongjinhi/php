43cbdd65ed9ff3b6078181556a6eb7d79b19f914
tongjinhi <148730050+tongjinhi@users.noreply.github.com>
2024-11-19 15:36:02 +0900

    Add files via upload

--- 20415/20415_burger.sql
+++ 20415/20415_burger.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- 생성 시간: 24-10-21 03:02
--- 서버 버전: 10.4.32-MariaDB
--- PHP 버전: 8.2.12
+-- 생성 시간: 24-11-19 07:32
+-- 서버 버전: 10.4.27-MariaDB
+-- PHP 버전: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,6 +32,7 @@ CREATE TABLE `20415_burger` (
   `pay` int(11) NOT NULL,
   `R_day` date NOT NULL,
   `detail` char(20) NOT NULL,
+  `calorie` int(11) NOT NULL,
   `image` char(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -39,8 +40,12 @@ CREATE TABLE `20415_burger` (
 -- 테이블의 덤프 데이터 `20415_burger`
 --
 
-INSERT INTO `20415_burger` (`name`, `pay`, `R_day`, `detail`, `image`) VALUES
-('더블치즈베이컨 버거', 4000, '2030-04-01', '고기 양파 치즈 빵', 'buger.png');
+INSERT INTO `20415_burger` (`name`, `pay`, `R_day`, `detail`, `calorie`, `image`) VALUES
+('감자튀김', 2500, '2030-04-01', '기름,감자', 330, 'french fries.jpg'),
+('더블치즈베이컨 버거', 4000, '2030-04-01', '고기,양파,치즈,빵,토마토', 2000, 'buger.jpg'),
+('초코콘', 1000, '2030-04-01', '초콜릿,아이스크림,콘', 500, 'Choco cone.jpg'),
+('치킨너겟', 2300, '2030-04-01', '닭고기,대두,밀,우유,쇠고기,달걀', 1800, 'Chicken Nuggets.jpg'),
+('콜라', 2000, '2030-04-01', '설탕,물,인산,색소', 350, 'cola.jpg');
 
 --
 -- 덤프된 테이블의 인덱스
