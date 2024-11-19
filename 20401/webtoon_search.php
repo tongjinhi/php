2f99e52aab92b0276f8aa95bf8ff3afd27189710
tongjinhi <148730050+tongjinhi@users.noreply.github.com>
2024-11-19 16:17:10 +0900

    Update webtoon_search.php

--- 20401/webtoon_search.php
+++ 20401/webtoon_search.php
@@ -3,7 +3,7 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <link type = "text/css" rel="stylesheet" href="../book.css">
+        <link type = "text/css" rel="stylesheet" href="book.css">
     </head>
     <body>
         <h1> 웹툰 검색 </h1>
