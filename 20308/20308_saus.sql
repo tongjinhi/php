8f6775a0aa53854dd4a63cb77f1d736d08fc8642
tongjinhi <148730050+tongjinhi@users.noreply.github.com>
2024-11-20 13:01:49 +0900

    Add files via upload

--- 20308/20308_saus.sql
+++ 20308/20308_saus.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- 생성 시간: 24-10-21 02:52
--- 서버 버전: 10.4.32-MariaDB
--- PHP 버전: 8.2.12
+-- 생성 시간: 24-11-20 03:48
+-- 서버 버전: 10.4.27-MariaDB
+-- PHP 버전: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -41,7 +41,11 @@ CREATE TABLE `20308_saus` (
 --
 
 INSERT INTO `20308_saus` (`project_name`, `budget`, `com_name`, `con_name`, `email`, `image`) VALUES
-('마케팅 강의', '500만원', 'erusau', '김수혁', 'gims49319@gmail.com', 'project.jpg');
+('보험왕이 알려주는 영업 강의', '500만원', 'KCS', '김민주', '24tj20308@tongjin.hs.kr', '보험왕 영업.jpeg'),
+('상대방을 신경쓰며 거절하는 방법 강의', '200만원', 'ERUS', '만구자', '24tj20308@tongjin.hs.kr', '거절 잘 하는 방법.jpeg'),
+('아이스 브레이킹을 잘하는 방법 강의', '100만원', 'IBSG', '강설파', '24tj20308@tongjin.hs.kr', '말을 잘하는 방법.jpeg'),
+('영업자의 태도 교육', '50만원', 'SAT', '이준', '24tj20308@tongjin.hs.kr', '영업사원의 태도.jpeg'),
+('통진고 학생들을 위한 마케팅 강의', '10만원', '통진고', '김수혁', '24tj20308@tongjin.hs.kr', '통진고 마케팅 강의.jpeg');
 
 --
 -- 덤프된 테이블의 인덱스
