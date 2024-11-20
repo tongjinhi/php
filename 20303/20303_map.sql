2121f43744b9107f53b3cc652972d632ce36b796
tongjinhi <148730050+tongjinhi@users.noreply.github.com>
2024-11-20 12:59:06 +0900

    Add files via upload

--- 20303/20303_map.sql
+++ 20303/20303_map.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- 생성 시간: 24-10-21 02:45
--- 서버 버전: 10.4.32-MariaDB
--- PHP 버전: 8.2.12
+-- 생성 시간: 24-11-20 03:52
+-- 서버 버전: 10.4.27-MariaDB
+-- PHP 버전: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -41,7 +41,11 @@ CREATE TABLE `20303_map` (
 --
 
 INSERT INTO `20303_map` (`name`, `maker`, `version`, `map_date`, `description`, `image`) VALUES
-('점프맵', '스티브', '1.150.2', '2022-02-22', '다양한 컨셉의 지형을 뛰어넘으며 자신의 실력을 확인하세요!', 'jump_map.png');
+('colorless', '재민1414', '1.20', '2024-07-07', '색이 사라진 세계에서 색을 다시 되찾기 위해 모험을 떠나는 주인공의 이야기', 'colorless.png'),
+('the backroom', 'WAL', '1.20.1', '2024-07-07', '백룸에서부터 탈출하세요!', 'backroom.png'),
+('time keeper', '리스빈', '1.13.3', '2024-07-07', '시간을 지키는 타임키퍼의 위대한 여정!', 'time keeper.png'),
+('수많은 별들 중에', '콜룰라', '1.12.2', '2024-07-07', '로맨스 소설', 'stars.png'),
+('팬션 살인사건', '별돌맹이', '1.7.10', '2024-07-07', '팬션 살인사건 과연 범인은?', 'merder.png');
 
 --
 -- 덤프된 테이블의 인덱스
