244fe54928b3381b625865d0a1a34dbfb8473dd6
tongjinhi <148730050+tongjinhi@users.noreply.github.com>
2024-11-21 16:32:18 +0900

    Add files via upload

---
+++ 20310/lc_search.php
@@ -0,0 +1,26 @@
+<?php include "../db.php";?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link type="text/css" rel="stylesheet" href="../book.css">
+    </head>
+    <body>
+        <h1> 크리쳐 확인 폼 </h1>
+        <div style="text-align: center;">
+            <form action="lc_search_ok.php" method="POST">
+                <fieldset>
+                    <select name="search_option">
+                        <option value="fcreture_name">     크리쳐 이름 </option>
+                        <option value="fkillability">    처치여부 </option>
+                        <option value="fattack"> 공격력 </option>
+                        <option value="fmax">    스폰수 </option>
+                        <option value="fbehavier">   행동 </option>
+                    </select>
+                    <input type="text" name="search_word">
+                    <input type="submit" value="검색">
+                </fieldset>
+            </form>
+        </div>
+    </body>
+</html>
