17b672ecdc3c55405e33cf0105d8484b69b80dc3
tongjinhi <148730050+tongjinhi@users.noreply.github.com>
2024-11-21 13:57:00 +0900

    Add files via upload

---
+++ 20401/webtoon_search_ok.php
@@ -0,0 +1,20 @@
+<?php include "../db.php"; ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link type = "text/css" rel="stylesheet" href="../book.css">
+    </head>
+    <body>
+        <h1> 검색 결과 </h1>
+        <table border='1' align='center'>
+            <tr> <td rowspan='5'> <img src='./img/이제 곧 죽습니다.jpg' height='150'> </td>
+                 <td> 제목 </td> <td> 이제 곧 죽습니다 </td> </tr>
+            <tr> <td> 작가 </td> <td> 이원식 </td> </tr>
+            <tr> <td> 장르 </td> <td> 판타지 </td> </tr>
+            <tr> <td> 연재 기간 </td> <td> 2019-04-18 </td> </tr>
+            <tr> <td> 연재 주기 </td> <td> 금요일 </td> </tr>
+        </table>
+        <a href='webtoon_search.php'> 웹툰 검색 </a>
+    </body>
+</html>
