7849b76029368a619d58374f7d23ad701819fe41
tongjinhi <148730050+tongjinhi@users.noreply.github.com>
2024-11-21 16:33:13 +0900

    Add files via upload

---
+++ 20323/pc_search.php
@@ -0,0 +1,26 @@
+<?php include "../db.php";?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charet = "utf-8" />
+        <link type = "text/css" rel = "stylesheet" href = "../book.css">
+    </head>
+    <body>
+        <h1> 컴퓨터 사양 검색 </h1>
+        <div style = "text-align: center;">
+            <form action = "pc_search_ok.php" method = "POST">
+                <fieldset>
+                    <select name = "seatch_option">
+                        <option value = "game">게임 이름</option>
+                        <option value = "goal">목적</option>
+                        <option value = "performance">성능</option>
+                        <option value = "cpu_br">cpu브랜드</option>
+                        <option value = "pc_price">컴퓨터 가격</option>
+                    </select>
+                    <input type = "text" name = "search_word">
+                    <input type = "submit" value = "검색">
+                </fieldset>
+            </form>
+        </div>
+    </body>
+</html>
---
+++ 20323/pc_search_ok.php
@@ -0,0 +1,22 @@
+<?php include "../db.php";?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link type="text/css" rel="stylesheet" href="../book.css">
+    </head>
+    <body>
+        <h1> 검색 결과 </h1>
+
+        <table border='1' align='center'>
+            <tr> <td rowspan='5'> <img src='./img/오버워치.jpg' height='150'> </td>
+                 <td> 게임 이름 </td> <td> 오버워치      </td> </tr>
+            <tr> <td> 목적 </td> <td> 게임용  </td> </tr>
+            <tr> <td> 성능 </td> <td> 중  </td> </tr>
+            <tr> <td> cpu브랜드 </td> <td> 인텔 </td> </tr>
+            <tr> <td> 컴퓨터 가격 </td> <td> 800000 </td> </tr>
+        </table>
+        
+        <a href='main_search.php'> 컴퓨터 사양 검색</a>
+    </body>
+</html>
