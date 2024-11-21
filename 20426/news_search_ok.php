1cb657ed7ff112d5c28876329a73de8242ed235c
tongjinhi <148730050+tongjinhi@users.noreply.github.com>
2024-11-21 14:00:09 +0900

    Add files via upload

---
+++ 20426/news_search_ok.php
@@ -0,0 +1,22 @@
+<?php include "../db.php";?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link type="text/css" rel="stylesheet" href="../book.css">
+    </head>
+    <body>
+        <h1> 경영 뉴스 결과 </h1>
+
+        <table border='1' align='center'>
+            <tr> <td rowspan='5'> <img src='./img/ESG.jpg' height='150'> </td>
+                 <td> 제목 </td> <td> ESG A등급 받은 비결      </td> </tr>
+            <tr> <td> 뉴스 날짜 </td> <td> 2023-09-24 </td> </tr>
+            <tr> <td> 기자 의견 </td> <td> 예술 작품 탄생  </td> </tr>
+            <tr> <td> 기사 내용 </td> <td> 6년 연속 A등급 </td> </tr>
+            <tr> <td> 출처 </td> <td> 뉴스투데이=김소희 기자 </td> </tr>
+        </table>
+        
+        <a href='news_search.php'> 경영 뉴스 검색</a>
+    </body>
+</html>
