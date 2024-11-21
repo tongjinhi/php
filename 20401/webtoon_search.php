80163245cfe5317384ba89812dc4fffbff9c2e9c
tongjinhi <148730050+tongjinhi@users.noreply.github.com>
2024-11-22 07:57:38 +0900

    Update webtoon_search.php

--- 20401/webtoon_search.php
+++ 20401/webtoon_search.php
@@ -8,7 +8,7 @@
     <body>
         <h1> 웹툰 검색 </h1>
         <div style="text-align: center;">
-            <form action="webtoon_search_ok.php" method="POST">
+            <form action="webtoon_search_ok.php" method="GET">
                 <fieldset>
                     <select name = "search_option">
                         <option value="title"> 제목 </option>
