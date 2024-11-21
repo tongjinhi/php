6c2806345c4af49acc32cf18e704c147e33500f3
tongjinhi <148730050+tongjinhi@users.noreply.github.com>
2024-11-21 13:57:49 +0900

    Add files via upload

---
+++ 20402/artistsearch_search_ok.php
@@ -0,0 +1,22 @@
+<?php include "../db.php";?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link type="text/css" rel="stylesheet" href="../book.css">
+    </head>
+    <body>
+        <h1> 검색 결과 </h1>
+
+        <table border='1' align='center'>
+            <tr> <td rowspan='5'> <img src='./img/Pablo_picasso_1.jpg' height='150'> </td>
+                 <td> 아이디 </td> <td>    1     </td> </tr>
+            <tr> <td> 시리즈 </td> <td> 시녀들   </td> </tr>
+            <tr> <td> 영향 </td> <td> 빈첸트 반 고흐  </td> </tr>
+            <tr> <td> 시대 </td> <td> 입제추의 </td> </tr>
+            <tr> <td> 국적 </td> <td> 스페인 </td> </tr>
+        </table>
+        
+        <a href='artist_search.php'> 아티스트 검색</a>
+    </body>
+</html>
