1b57f3a7132e8491efd316d0b4d38d7a952ee8d5
tongjinhi <148730050+tongjinhi@users.noreply.github.com>
2024-11-22 07:59:30 +0900

    Update burger_search.php

--- 20415/burger_search.php
+++ 20415/burger_search.php
@@ -8,7 +8,7 @@
     <body>
         <h1> 버거 검색 </h1>
         <div style="text-aling: center;">
-            <form action="main_search_ok.php" method="POST">
+            <form action="main_search_ok.php" method="GET">
                 <fieldset>
                     <select name="search_option">
                         <option value="name"> 버거이름</option>
