8cf6b84ad8d694ca0dcfbd961d336fd10815a7dc
tongjinhi <148730050+tongjinhi@users.noreply.github.com>
2024-11-22 07:59:56 +0900

    Update store_search.php

--- 20418/store_search.php
+++ 20418/store_search.php
@@ -8,7 +8,7 @@
     <body>
         <h1>옷 검색</h1>
         <div style="text-allign: center;">
-            <from action="main_search_ok.php" method="POST">
+            <from action="main_search_ok.php" method="GET">
                 <fieldset>
                     <select name="search_option">
                         <option value="prod_num"> 품번 </option>
