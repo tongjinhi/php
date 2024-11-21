d1b640fcc01ef68fe4bbe0cb14d5b6788a050851
tongjinhi <148730050+tongjinhi@users.noreply.github.com>
2024-11-21 14:02:32 +0900

    Add files via upload

---
+++ 20410/food_search_ok.php
@@ -0,0 +1,22 @@
+<?php include "../db.php";?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link type="text/css" rel="stylesheet" href="../book.css">
+    </head>
+    <body>
+        <h1> 검색 결과 </h1>
+
+        <table border='1' align='center'>
+            <tr> <td rowspan='5'> <img src='./img/egg_in_hell.jpg' height='150'> </td>
+                 <td> 이름 </td> <td> 에그 인 헬(샥슈카)      </td> </tr>
+            <tr> <td> 난이도 </td> <td> 3  </td> </tr>
+            <tr> <td> 재료 </td> <td> 계란, 토마토 소스, 소세지 ...  </td> </tr>
+            <tr> <td> 레쉬피 </td> <td> 먹기 좋게 소세지를 잘라 ... </td> </tr>
+            <tr> <td> 요리 설명 </td> <td> 이 음식은 지중해의 ... </td> </tr>
+        </table>
+        
+        <a href='food_search.php'> 요리 검색</a>
+    </body>
+</html>
