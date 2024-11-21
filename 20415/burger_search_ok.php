9c093c0e2aa08258b3d7a8b6ecf22ef52fd46fbc
tongjinhi <148730050+tongjinhi@users.noreply.github.com>
2024-11-21 14:01:20 +0900

    Add files via upload

---
+++ 20415/burger_search_ok.php
@@ -0,0 +1,22 @@
+<?php include "../db.php";?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link type="text/css" rel="stylesheet" href="../book.css">
+    </head>
+    <body>
+        <h1> 검색 결과 </h1>
+
+        <table border='1' align='center'>
+            <tr> <td rowspan='5'> <img src='./img/데미안.jpg' height='150'> </td>
+                 <td> 도서명 </td> <td> 데미안      </td> </tr>
+            <tr> <td> 저자명 </td> <td> 헤르만헤세  </td> </tr>
+            <tr> <td> 출판사 </td> <td> 통진고출판  </td> </tr>
+            <tr> <td> 출판일 </td> <td> 2024-08-12 </td> </tr>
+            <tr> <td> 책설명 </td> <td> 참다운 어른이 되어 가는 소년 싱클레어의 이야기 </td> </tr>
+        </table>
+        
+        <a href='main_search.php'> 도서 검색</a>
+    </body>
+</html>
