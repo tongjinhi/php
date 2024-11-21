2d75b85a098d94a5ee543cba77179433d6d60f91
tongjinhi <148730050+tongjinhi@users.noreply.github.com>
2024-11-21 14:04:18 +0900

    Add files via upload

---
+++ 20407/stock_search_ok.php
@@ -0,0 +1,22 @@
+<?php include "../db.php";?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link type="text/css" rel="stylesheet" href="../book.css">
+    </head>
+    <body>
+        <h1> 검색 결과 </h1>
+
+        <table border='1' align='center'>
+            <tr> <td rowspan='5'> <img src='./img/zhzk.jpeg' height='150'> </td>
+                 <td> 주식명 </td> <td> 코카콜라      </td> </tr>
+            <tr> <td> 주식정보 </td> <td> 펩시를 이길 수 있을지? </td> </tr>
+            <tr> <td> 그래프 </td> <td> 반등좀 있음  </td> </tr>
+            <tr> <td> 주식차트 </td> <td> 이미지</td> </tr>
+            <tr> <td> 나라 </td> <td> 미국 </td> </tr>
+        </table>
+        
+        <a href='stock_search.php'> 주식 검색</a>
+    </body>
+</html>
