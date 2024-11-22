e797fd3a2fba3d770a31b2c7ba7904817055a99e
tongjinhi <148730050+tongjinhi@users.noreply.github.com>
2024-11-22 13:43:12 +0900

    Add files via upload

--- 20409/movie_search_ok.php
+++ 20409/movie_search_ok.php
@@ -8,15 +8,30 @@
     <body>
         <h1> 영화 결과 </h1>
 
-        <table border='1' align='center'>
-            <tr> <td rowspan='5'> <img src='./img/날씨의 아이.jpg' height='150'> </td>
-                 <td> 영화이름 </td> <td> 날씨의 아이      </td> </tr>
-            <tr> <td> 감독 </td> <td> 신카이 마코토  </td> </tr>
-            <tr> <td> 장르 </td> <td> 로맨스/재난  </td> </tr>
-            <tr> <td> 등급 </td> <td> 15세이상 관람가 </td> </tr>
-            <tr> <td> 개봉일 </td> <td> 2024-07-19 </td> </tr>
-        </table>
+        <?php
+        $search_option = $_GET['search_option'];
+        $search_word   = $_GET['search_word'];
+        echo $search_option . "," . $search_word;
+
+        $sql = null;
+        if($search_word == "")
+            $sql = "select * from 20409_movie";
+        else
+            $sql = "select * from 20409_movie where {$search_option} like '%{$search_word}%'";
         
+        $rs = my_query($sql);
+
+        echo "<table border='1' align='center'>";
+        foreach($rs as $field) {
+            echo "    <tr> <td rowspan='5'> <img src='./img/{$field['image']}' height='150'> </td>";
+            echo "         <td> 영화이름 </td> <td> {$field['movie_name']}      </td> </tr>";
+            echo "    <tr> <td> 감독 </td> <td> {$field['supervision']}  </td> </tr>";
+            echo "    <tr> <td> 장르 </td> <td> {$field['genre']}  </td> </tr>";
+            echo "    <tr> <td> 등급 </td> <td> {$field['rating']} </td> </tr>";
+            echo "    <tr> <td> 개봉일 </td> <td> {$field['opening_date']} </td> </tr>";
+        }
+        echo "</table>";
+        ?>
         <a href='movie_search.php'> 영화 검색</a>
     </body>
 </html>
