4b554bb7dca1d9cefcb89d61a0a63f241df614dc
tongjinhi <148730050+tongjinhi@users.noreply.github.com>
2024-11-22 13:43:50 +0900

    Add files via upload

--- 20426/news_search_ok.php
+++ 20426/news_search_ok.php
@@ -8,15 +8,31 @@
     <body>
         <h1> 경영 뉴스 결과 </h1>
 
-        <table border='1' align='center'>
-            <tr> <td rowspan='5'> <img src='./img/ESG.jpg' height='150'> </td>
-                 <td> 제목 </td> <td> ESG A등급 받은 비결      </td> </tr>
-            <tr> <td> 뉴스 날짜 </td> <td> 2023-09-24 </td> </tr>
-            <tr> <td> 기자 의견 </td> <td> 예술 작품 탄생  </td> </tr>
-            <tr> <td> 기사 내용 </td> <td> 6년 연속 A등급 </td> </tr>
-            <tr> <td> 출처 </td> <td> 뉴스투데이=김소희 기자 </td> </tr>
-        </table>
-        
+        <?php
+        $search_option = $_GET['search_option'];
+        $search_word = $_GET['search_word'];
+        echo $search_option . "," . $search_word;
+
+        $sql = null;
+        if($search_word == "")
+            $sql = "select * from 20426_news";
+        else
+            $sql = "select * from 20426_news where {$search_option} like '%{$search_word}%'";
+
+        $rs = my_query($sql);
+
+
+        echo "<table border='1' align='center'>";
+        foreach($rs as $field) {
+            echo "    <tr> <td rowspan='5'> <img src='./img/{$field['image']}' height='150'> </td>";
+            echo "         <td> 제목 </td>  <td> {$field['title']}      </td> </tr>";
+            echo "    <tr> <td> 뉴스 날짜 </td> <td> {$field['news_date']} </td> </tr>";
+            echo "    <tr> <td> 기자 의견 </td> <td> {$field['opinion']}  </td> </tr>";
+            echo "    <tr> <td> 기사 내용 </td> <td> {$field['contect']} </td> </tr>";
+            echo "    <tr> <td> 출처 </td> <td> {$field['source']} </td> </tr>";
+        }
+        echo "</table>";
+        ?>
         <a href='news_search.php'> 경영 뉴스 검색</a>
     </body>
 </html>
