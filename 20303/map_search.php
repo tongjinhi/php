ad6eaaae461acdce1289e1f87f127cfc5a37fa99
tongjinhi <148730050+tongjinhi@users.noreply.github.com>
2024-11-22 09:28:10 +0900

    Add files via upload

---
+++ 20303/map_search.php
@@ -0,0 +1,26 @@
+<?php include "../db.php";?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link type="text/css" rel="stylesheet" href="../book.css">
+    </head>
+    <body>
+        <h1> 맵 검색 </h1>
+        <div style="text-align: center;">
+            <form action="map_search_ok.php" method="GET">
+                <fieldset>
+                    <select name="search_option">
+                        <option value="name">        이름 </option>
+                        <option value="maker">       만든사람 </option>
+                        <option value="version">     버젼 </option>
+                        <option value="map_date">    만든 날짜 </option>
+                        <option value="discription">  설명 </option>
+                    </select>
+                    <input type="text" name="search_word">
+                    <input type="submit" value="검색">
+                </fieldset>
+            </form>
+        </div>
+    </body>
+</html>
