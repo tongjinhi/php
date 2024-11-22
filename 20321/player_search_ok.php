8f45b96dc74c30c7dbfe9acdda3661a1c59873ce
tongjinhi <148730050+tongjinhi@users.noreply.github.com>
2024-11-22 09:26:49 +0900

    Add files via upload

---
+++ 20321/player_search_ok.php
@@ -0,0 +1,22 @@
+<?php include "../db.php";?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link type="text/css" rel="stylesheet" href="../book.css">
+    </head>
+    <body>
+        <h1> 선수 검색 결과 </h1>
+
+        <table border='1' align='center'>
+            <tr> <td rowspan='5'> <img src='./img/케빈 더브라위너.jpg.jpg' height='150'> </td>
+                 <td> 선수 이름 </td> <td> 캐빈 더브라위너      </td> </tr>
+            <tr> <td> 소속팀 </td> <td> 맨체스터 시티FC  </td> </tr>
+            <tr> <td> 국적 </td> <td> 벨기에  </td> </tr>
+            <tr> <td> 포지션 </td> <td> 공격형 미드필더 </td> </tr>
+            <tr> <td> 설명 </td> <td> EPL최다 도움왕(4회) </td> </tr>
+        </table>
+        
+        <a href='player_search.php'> 선수 검색</a>
+    </body>
+</html>
