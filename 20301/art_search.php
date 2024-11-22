ad80c08a097fbf659087ecfa4f08f6c823adc7bd
tongjinhi <148730050+tongjinhi@users.noreply.github.com>
2024-11-22 09:54:20 +0900

    Add files via upload

--- 20301/art_search.php
+++ 20301/art_search.php
@@ -8,19 +8,19 @@
     <body>
         <h1> 작품 검색 </h1>
         <div style="text-align: center;">
-            <from action="main_search_ok.php" method="POST">
+            <form action="art_search_ok.php" method="GET">
                 <fieldset>
                     <select name="search_option">
-                        <option value="name">      작품명   </option>
-                        <option value="writer">    저자명   </option>
-                        <option value="publisher"> 출판사   </option>
-                        <option value="p_date">    출판일   </option>
-                        <option value="summary">   작품설명 </option>
+                        <option value="art_name"> 작품명 </option>
+                        <option value="author"> 작가 </option>
+                        <option value="publisher"> 출판사 </option>
+                        <option value="art_date"> 출판일 </option>
+                        <option value="art_description"> 작품설명 </option>
                     </select>
                     <input type="text" name="search_word">
                     <input type="submit" value="검색">
                 </fieldset>
-            </from>
+            </form>
         </div>
     </body>
 </html>
