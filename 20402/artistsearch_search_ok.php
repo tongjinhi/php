d0e6b16bebbde7ae7425953e026e676e42d65257
tongjinhi <148730050+tongjinhi@users.noreply.github.com>
2024-11-22 13:42:40 +0900

    Add files via upload

--- 20402/artistsearch_search_ok.php
+++ 20402/artistsearch_search_ok.php
@@ -8,15 +8,30 @@
     <body>
         <h1> 검색 결과 </h1>
 
-        <table border='1' align='center'>
-            <tr> <td rowspan='5'> <img src='./img/Pablo_picasso_1.jpg' height='150'> </td>
-                 <td> 아이디 </td> <td>    1     </td> </tr>
-            <tr> <td> 시리즈 </td> <td> 시녀들   </td> </tr>
-            <tr> <td> 영향 </td> <td> 빈첸트 반 고흐  </td> </tr>
-            <tr> <td> 시대 </td> <td> 입제추의 </td> </tr>
-            <tr> <td> 국적 </td> <td> 스페인 </td> </tr>
-        </table>
-        
+        <?php 
+        $search_option = $_GET['search_option'];
+        $search_word = $_GET['search_word'];
+        echo $search_option . "," . $search_word;
+
+        $sql = null;
+        if($search_word == "")
+            $sql = "select * from 20402_artistsearch";
+        else
+            $sql = "select * from 20402_artistsearch where {$search_option} like '%{$search_word}%'";
+
+        $rs = my_query($sql);
+
+        echo"<table border='1' align='center'>";
+        foreach($rs as $field) {
+        echo"    <tr> <td rowspan='5'> <img src='./img/{$field['image']}' height='150'> </td>";
+        echo"         <td> 아이디 </td> <td> {$field['person_id']}     </td> </tr>";
+        echo"    <tr> <td> 시리즈 </td> <td> {$field['series']}           </td> </tr>";
+        echo"    <tr> <td> 영향 </td> <td> {$field['influence']}      </td> </tr>";
+        echo"    <tr> <td> 시대 </td> <td> {$field['times']}            </td> </tr>";
+        echo"    <tr> <td> 국적 </td> <td> {$field['nationality']}             </td> </tr>";
+    }
+        echo"</table>";
+        ?>
         <a href='artist_search.php'> 아티스트 검색</a>
     </body>
 </html>
