7d9b9d04bfbbd4bbf39912ce635a65037dde165f
tongjinhi <148730050+tongjinhi@users.noreply.github.com>
2024-12-05 08:38:46 +0900

    Update login_ok.php

--- 20034/login/login_ok.php
+++ 20034/login/login_ok.php
@@ -3,14 +3,14 @@
         echo '<script> alert("아이디나 패스워드 입력하세요");
         history.back(); </script>';
     } else {
-        $input_pass = $_POST['userpw'];
+        $password = $_POST['userpw'];
         $sql = my_query("select * from testmember 
                          where id = '".$_POST['userid']."'");
         $member = $sql->fetch_array();
         #echo $member['id'] . "," . $member['pass'];
         
-        $db_pass = $member['pass'];
-        if($input_pass == $db_pass){
+        $hash_pw = $member['pass'];
+        if($password == $hash_pw){
             $_SESSION['userid'] = $member['id'];
             $_SESSION['userpw'] = $member['pass'];
             echo "<script> alert('로그인 되었습니다.');
