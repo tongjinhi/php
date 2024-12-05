2b8fa34cde410bf56b14aa768918777d3d37aa2e
tongjinhi <148730050+tongjinhi@users.noreply.github.com>
2024-12-05 18:03:12 +0900

    Update index.php

--- 20034/login/index.php
+++ 20034/login/index.php
@@ -7,19 +7,20 @@
     <body>
         <div id="login_box">
             <h1>로그인</h1>
-            <form method="post" action="login_ok.php">
+            <form method="post" action="login_ok.php" id="login_form">
                 <table border="1" cellspacing="0" width="300">
                     <tr>
                         <td width="130">
-                            <input type="text" name="userid" class="inph">
+                            <input type="text" name="userid" id="id">
                         </td>
                         <td rowspan="2" align="center" width="100" >
                             <button type="submit" id="btn" >로그인</button>
+                            <!--input type="button" value="확인후 로그인"-->
                         </td>
                     </tr>
                     <tr>
                         <td width="130">
-                            <input type="password" name="userpw" class="inph">
+                            <input type="password" name="userpw" id="pwd">
                         </td>
                     </tr>
                     <tr>
